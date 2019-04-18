dbf3fec8191af03992e008d5860f296a4e2ee0ae
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-04-18 13:17:21 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@
   //$(".wrapper").html("<p>Trial</p>");
 //});
 
-//$(".login-btn").click(function(){
-  //alert("click");
-//});
+$(".login-btn").click(function(){
+  alert("click");
+});
 
-function myFunction(){
-  alert("clicked once");
-}
+//function myFunction(){
+  //alert("clicked once");
+//}
