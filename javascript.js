20fe7eb4fcd221e40c97833edfd660baa3989cce
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-04-18 20:28:10 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -52,7 +52,7 @@ $(function(){
   
   $(".login").click(function(){
     $.ajax({url: "https://kyclassroom.github.io/test.py", success: function(result){
-      $(".wrapper").html(result);
+      $(".wrapper").val(result);
     }});
   });
   
