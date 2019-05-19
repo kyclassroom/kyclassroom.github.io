af9f521a8948ee3a6461fb299cbe03d3b3d22087
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-05-20 02:03:25 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
+function general_click(str1, str2, str3) {
+  $("." + str1 + str2).click(function(){
+    $("." + str1 + str3).toggleClass("d-none");
+  });
+}
+
+function btn_click(str) {
+  general_click(str, "_btn", "_text");
+}
+
 $(function(){
-
-  // jQuery methods go here...
-
+  btn_click("angle_between_two_planes");
+  general_click("angle_between_two_planes", "_hint", "_content");
+  btn_click("distance_formula");
+  btn-click("in-centre_formula");
+  btn-click("recurrence");
 });
