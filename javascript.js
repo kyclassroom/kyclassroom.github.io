856077b5b49891c24866c6e9d0df61e01562ce9b
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-05-20 03:23:15 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,18 +8,15 @@ function btn_click(str) {
   general_click(str, "_btn", "_text");
 }
 
+function hint_click(str) {
+  general_click(str, "_hint", "_content");
+}
+
 $(function(){
   btn_click("angle_between_two_planes");
-  general_click("angle_between_two_planes", "_hint", "_content");
+  hint_click("angle_between_two_planes");
   btn_click("distance_formula");
+  hint_click("distance_formula");
   btn_click("in-centre_formula");
   btn_click("recurrence");
-  btn_click("tanget_half_angle_formulas");
-  btn_click("first_principles");
-  btn_click("curve_skteching");
-  btn_click("t_formulas");
-  btn_click("ellipse");
-  btn_click("eigenvalue");
-  btn_click("reduced_row_echelon_form");
-  btn_click("normal");
 });
