1adcda00a4280cba41420882527cfc6286c369fd
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-06-24 14:17:02 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,10 @@ $(function(){
   
   var previous_eng = '';
   setInterval(function() {
+    console.log("test")
     eng = $("#eng").val()
     if (eng != previous_eng) {
+      console.log("test_alt")
       previous_eng = eng;
       $(".latex_eng").html(eng);
     }
