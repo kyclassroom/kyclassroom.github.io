eabde97edae86bcc0d027d0d249f8ce70ea8e05f
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-06-24 14:19:40 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 $(function(){
   
-  var previous_eng = '';
+  var previous_eng = "";
   setInterval(function() {
-    console.log("test")
     eng = $("#eng").val()
     if (eng != previous_eng) {
-      console.log("test_alt")
       previous_eng = eng;
       $(".latex_eng").html(eng);
+    }
+  }, 10);
+  
+  var previous_chi = "";
+  setInterval(function() {
+    chi = $("#chi").val()
+    if (chi != previous_chi) {
+      previous_eng = chi;
+      $(".latex_eng").html(chi);
     }
   }, 10);
   
