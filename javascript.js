9f6fe0ad0b435e946414b6e3f4ee955e98d41cd3
kyclassroom <49487898+kyclassroom@users.noreply.github.com>
2019-06-25 14:27:29 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,3 +15,22 @@ $(".submit").click(function(){
     submit_chi($(this).html());
   });
 });
+
+$("button").click(function(){
+var MyHeaderss = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.32 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.32",
+              "X-GWT-Permutation" : "6FEFBE57C6E73F0AB33BD5A4E17945DE",
+              "Content-Type":"text/x-gwt-rpc; charset=utf-8",
+              "X-GWT-Module-Base": "https://www.cartetitolari.mps.it/portaleTitolari/",
+              "Referer": "https://www.cartetitolari.mps.it/portaleTitolari/titolari.html"};
+$.ajax({
+    type:"POST",
+    beforeSend: function (request)
+    {
+      request.setRequestHeader(MyHeaderss);
+    },
+    url: 'https://www.cartetitolari.mps.it/portaleTitolari/service',
+    data: login_data,
+    success: function(databak) {
+    }
+});
+});
